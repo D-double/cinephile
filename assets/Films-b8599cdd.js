ea7b8693037dec1b95c3d9b1d9a05b9d35202359
Abdulla <dream05@mail.ru>
2023-09-21 22:08:26 +0500

    changed router history

---
+++ assets/Films-b8599cdd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-94766679.js";import{o as a,c as o,a as s}from"./index-6fd466d6.js";const t={class:"main"},i={__name:"Films",setup(c){return(_,m)=>(a(),o("main",t,[s(e,{type:"movie"})]))}};export{i as default};
---
+++ assets/SerialId-df46d1ea.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Single-763007ac.js";import{o as a,c as t,a as o}from"./index-6fd466d6.js";const s={class:"main"},i={__name:"SerialId",setup(c){return(r,_)=>(a(),t("main",s,[o(e,{type:"tv"})]))}};export{i as default};
