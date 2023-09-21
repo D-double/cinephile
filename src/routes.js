026b312bcc70ecc1883530d0a61a3f68d599547c
Abdulla <dream05@mail.ru>
2023-09-21 21:58:26 +0500

    added all routes alias

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // there is also createWebHashHistory and createMemoryHistory
 import Home from '@/pages/Home.vue';
-
+let aliasGithub = '/cinephile';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', name: 'home', component: Home, alias: '/cinephile' },
-    {path: '/movie', name: 'movies', component: ()=> import('@/pages/Films.vue')},
-    {path: '/search', name: 'search', component: ()=> import('@/pages/Search.vue')},
-    {path: '/tv', name: 'tvs', component: ()=> import('@/pages/Serials.vue')},
-    {path: '/movie/:id', name: 'movieid', component: ()=> import('@/pages/FilmId.vue')},
-    {path: '/tv/:id', name: 'tvid', component: ()=> import('@/pages/SerialId.vue')},
+    {path: '/', name: 'home', component: Home, alias: aliasGithub },
+    {path: '/movie', name: 'movies', component: ()=> import('@/pages/Films.vue'), alias: aliasGithub + '/movie' },
+    {path: '/search', name: 'search', component: ()=> import('@/pages/Search.vue'), alias: aliasGithub + '/search'},
+    {path: '/tv', name: 'tvs', component: ()=> import('@/pages/Serials.vue'), alias: aliasGithub + '/tv'},
+    {path: '/movie/:id', name: 'movieid', component: ()=> import('@/pages/FilmId.vue'), alias: aliasGithub + '/movie/:id'},
+    {path: '/tv/:id', name: 'tvid', component: ()=> import('@/pages/SerialId.vue'), alias: aliasGithub + '/tv/:id'},
   ],
   linkActiveClass: 'active',
   scrollBehavior() {  
     return { top: 0 }
   },
-  fullPath: '/cinephile'
+  // fullPath: '/cinephile'
 })
 
 export default router;
