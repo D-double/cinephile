a14e4fc5ac68a5f536c910bf2aa671d5d7959874
Abdulla <dream05@mail.ru>
2023-09-21 21:30:23 +0500

    change router fullPath

--- assets/FilmId-7f05bb89.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./Single-3598bf65.js";import{o as a,c as o,a as t}from"./index-47f3baa4.js";const s={class:"main"},i={__name:"FilmId",setup(c){return(_,m)=>(a(),o("main",s,[t(e,{type:"movie"})]))}};export{i as default};
--- assets/SerialId-fdcb7ee5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./Single-3598bf65.js";import{o as a,c as t,a as o}from"./index-47f3baa4.js";const s={class:"main"},i={__name:"SerialId",setup(c){return(r,_)=>(a(),t("main",s,[o(e,{type:"tv"})]))}};export{i as default};
