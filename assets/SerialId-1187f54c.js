e7d354ded4215d6a0bccc23d03065ed46bf17783
Abdulla <dream05@mail.ru>
2023-09-27 20:03:41 +0500

    added path from ListItem, Top, SearchItem

---
+++ assets/SerialId-1187f54c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Single-e4ec04b0.js";import{o as a,c as t,a as o}from"./index-fea261e6.js";const s={class:"main"},i={__name:"SerialId",setup(c){return(r,_)=>(a(),t("main",s,[o(e,{type:"tv"})]))}};export{i as default};
