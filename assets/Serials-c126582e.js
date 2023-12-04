41803e4eb354489999e214afd7c0f7554bc6c4cf
Abdulla <dream05@mail.ru>
2023-12-04 19:16:03 +0500

    Finished adding photos

---
+++ assets/Serials-c126582e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-1cbc4b09.js";import{o as a,c as t,a as s}from"./index-10fd3ce4.js";const o={class:"main"},i={__name:"Serials",setup(c){return(r,_)=>(a(),t("main",o,[s(e,{type:"tv"})]))}};export{i as default};
